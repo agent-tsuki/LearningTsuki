f32471e6669405ccabb78f681d24a85d6e4267a1
Shivam <premashivam00113@gmail.com>
2025-09-08 22:26:11 +0530

    test

--- clss/whileloop.py
+++ clss/whileloop.py
@@ -1,6 +1,25 @@
 ### While loops working
-a = 2
-b = 0
 
-while a > b:
-    print(a, b)
+name = 2
+
+def while_loops():
+    a = 2
+    b = 0
+
+    while a > b:
+        print(a, b)
+        b += 1
+
+while_loops()
+print("******************")
+
+while_loops()
+
+print("******************")
+
+while_loops()
+
+print("******************")
+
+while_loops()
+
