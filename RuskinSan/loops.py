094abb00d81fc38820d83794a57325c01f768246
ruskin <ruskinromesh21@gmail.com>
2025-09-08 22:14:37 +0530

    ques

--- RuskinSan/loops.py
+++ RuskinSan/loops.py
@@ -1,4 +1,9 @@
-for i in range(1,10):
-    if(i%2==0):
-        print(i)
-    
+# for i in range(1,10):
+#     if(i%2==0):
+#         print(i)
+
+a=8
+b=2
+while(a%b==0):
+    print("even number")
+    b+=1
