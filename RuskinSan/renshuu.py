405a33c841da07a1bb81a51765718f62579c8cd8
ruskin <ruskinromesh21@gmail.com>
2025-09-11 21:51:59 +0530

    name

--- RuskinSan/renshuu.py
+++ RuskinSan/renshuu.py
@@ -1,5 +1,16 @@
-a=int(input("enter a numner"))
-if(a>=0):
-    print("positive")
-else:
-    print("negative")
+# a=int(input("enter a numner"))
+# if(a>=0):
+#     print("positive")
+# else:
+#     print("negative")
+
+# # print(10+5)
+# print ("hello",end="****  ")
+# print("world")
+
+# a=int(input("enter your marks"))
+# if((a==100)and(a>90)):
+#     print("pass")
+
+for i in range(1,10):
+    print(i)
