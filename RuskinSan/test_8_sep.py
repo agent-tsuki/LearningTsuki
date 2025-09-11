14f3f11ef6737b056350c677ea428708c6be0725
ruskin <ruskinromesh21@gmail.com>
2025-09-11 22:38:02 +0530

    name

--- RuskinSan/test_8_sep.py
+++ RuskinSan/test_8_sep.py
@@ -9,12 +9,19 @@
 
 # Q1. Operators
 # Write code to find the remainder when 17 is divided by 3.
-
+# a=17
+# b=3
+# c=a%b
+# print(c)
 
 
 # Q2. if / else
 # Write an if/else statement to check if a number stored in variable x is positive or negative.
-
+# a=int(input("enter a value:"))
+# if(a>=0):
+#     print("positive number")
+# else:
+#     print("negative")
 
 
 # Q3. elif
@@ -22,45 +29,65 @@
 # - If score >= 90, print "Grade A"
 # - If score >= 75, print "Grade B"
 # - Else, print "Grade C"
-
-
+# marks=float(input("enter your marks:"))
+# if(marks>=90):
+#     print("Grade A")
+# elif(marks>=75):
+#     print("Grade B")
+# else:
+#     print("Grade C")
 
 # Q4. for loop
 # Write a for loop to print numbers from 1 to 5.
+# for i in range(1,6):
+#     print(i)
 
 
 
 # Q5. while loop
 # Write a while loop to print numbers from 5 down to 1.
-
+# a=6
+# while(a>0):
+#     a-=1
+#     print(a)
 
 
 # Q6. list
 # Create a list of 3 fruits and print the first fruit.
-
+# fruits=["apple","banana","chiku","santra"]
+# print(fruits[0])
 
 
 # Q7. tuple
 # Create a tuple of 3 numbers and print the last number.
-
+# a=(5,6,9)
+# print(a[2])
 
 
 # Q8. set
 # Create a set of {1, 2, 3, 3, 2} and print it.
 # (Hint: sets automatically remove duplicates)
-
+# a={1,2,3}
+# print(a)
 
 
 # Q9. dictionary
 # Create a dictionary with keys: "name", "age" and values of your choice. Print the value of "name".
-
+# a={"name":"Ruskin",
+#       "Age":25,
+#       "place":"New delhi"
+# }
+# print(a["name"])
 
 
 # Q10. Mixed
 # Write a program that:
 # - Creates a list of numbers [1,2,3,4,5]
 # - Uses a for loop to print only the even numbers
-
+# a=[1,2,3,4,5]
+# for i in a:
+#     if(i%2==0):
+#         print(i)
 
 
 # ======================================
