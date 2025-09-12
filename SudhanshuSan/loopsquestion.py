0012969d66d19a47791385a0d832e7c235adb03a
sudhanshu <sudhanshunihon@gmail.com>
2025-09-13 01:01:09 +0900

    ques

--- SudhanshuSan/loopsquestion.py
+++ SudhanshuSan/loopsquestion.py
@@ -1,10 +1,13 @@
-# a=100
-# b=4
-# while a>b:
-#     print(a,b)
-#     b=b+1
-#     print(a,b)
-#     b=b+5
-#     print(a,b)
+def whilebb():
+    a=10
+    b=4
+    while a>b:
+        print(a,b)
+        b=b+1
+        print(a,b)
+        b=b+5
+        print(a,b)
 
+whilebb()
+whilebb()
 
