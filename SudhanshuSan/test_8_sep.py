11de00350ae02b722c4060af0c20ae8b5bbe9b9a
sudhanshu <sudhanshunihon@gmail.com>
2025-09-13 01:57:08 +0900

    test

--- SudhanshuSan/test_8_sep.py
+++ SudhanshuSan/test_8_sep.py
@@ -9,12 +9,18 @@
 
 # Q1. Operators
 # Write code to find the remainder when 17 is divided by 3.
-
+# a=3
+# b=17
+# print(b%a)
 
 
 # Q2. if / else
-# Write an if/else statement to check if a number stored in variable x is positive or negative.
-
+# # Write an if/else statement to check if a number stored in variable x is positive or negative.
+# a=int(input("number"))
+# if a>0:
+#     print("pos")
+# else:
+#     print("neg")
 
 
 # Q3. elif
@@ -22,46 +28,58 @@
 # - If score >= 90, print "Grade A"
 # - If score >= 75, print "Grade B"
 # - Else, print "Grade C"
-
+# input("write marks")
+# a=90
+# if a<90:
+#     print("A")
+# elif a>90 or a<75:
+#     print("b")
+# else: 
+#     print("fail")
 
 
 # Q4. for loop
 # Write a for loop to print numbers from 1 to 5.
-
+#for(1,2,3,4,5)
 
 
 # Q5. while loop
 # Write a while loop to print numbers from 5 down to 1.
-
+# while (5,4,3,2,1)
 
 
 # Q6. list
 # Create a list of 3 fruits and print the first fruit.
-
+# a=["apple","banana","orange"]
+# print(a[0])
 
 
 # Q7. tuple
 # Create a tuple of 3 numbers and print the last number.
-
+# a=(1,2,3)
+# print(a[2])
 
 
 # Q8. set
 # Create a set of {1, 2, 3, 3, 2} and print it.
 # (Hint: sets automatically remove duplicates)
-
-
+# a={1,2,3,3,2}
+# print(a)
 
 # Q9. dictionary
 # Create a dictionary with keys: "name", "age" and values of your choice. Print the value of "name".
 
-
+# nhi aata
 
 # Q10. Mixed
 # Write a program that:
 # - Creates a list of numbers [1,2,3,4,5]
 # - Uses a for loop to print only the even numbers
 
-
+# a=[1,2,3,4,5]
+# for b in a:
+#     if b%2==0:
+#         print(b)
 
 # ======================================
 # End of Test
