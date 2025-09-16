7daa29f1656c84b19586044885396e470773b82a
sudhanshu <sudhanshunihon@gmail.com>
2025-09-17 01:49:30 +0900

    index

---
+++ SudhanshuSan/index.py
@@ -0,0 +1,32 @@
+"""
+create a sting with max 10 length
+1) print length of string
+2) print first character
+3) print last character
+4) print srange up to 5 char
+5) skip 2 char and print elemet
+
+[x:y:z]
+"""
+a="abcdefghiji"
+#print(len(a))
+
+#print(a[0])
+
+#print(a[10])
+
+#print(a[0:5])
+
+#print(a[0:9:2])
+
+
+
+
+
+
+
+
+
+
+
+
