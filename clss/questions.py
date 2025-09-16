3b60ebc68e376d4d9d3753b3c3a0726a4634a90a
Shivam <premashivam00113@gmail.com>
2025-09-16 21:59:24 +0530

    questions

---
+++ clss/questions.py
@@ -0,0 +1,10 @@
+"""
+create a sting with max 10 length
+1) print length of string
+2) print first character
+3) print last character
+4) print srange up to 5 char
+5) skip 2 char and print elemet
+
+[x:y:z]
+"""
