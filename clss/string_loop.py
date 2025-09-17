874b88292364d9ea489b5f4f76339ccc5376cdae
Shivam <premashivam00113@gmail.com>
2025-09-17 21:25:34 +0530

    sting loops

---
+++ clss/string_loop.py
@@ -0,0 +1,7 @@
+"""
+1) write a sting and convert to upper case and lower case
+2) write a string and reverse it HELP: use -1
+3) write a string and print length of string
+4) this is Sting names = "ShivamPythonSudhanshuPythonRuskinPython" split it using "Python" 
+5) Print all element of string using for loops
+"""
