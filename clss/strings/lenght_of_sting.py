b314b1054c85662042f9b7aa5918441b1d7b0e6e
Shivam <premashivam00113@gmail.com>
2025-09-28 22:13:40 +0530

    string fixed

---
+++ clss/strings/lenght_of_sting.py
@@ -0,0 +1,28 @@
+#### find length of sringg
+# strings = input("Enter a string: ")
+# c = 0
+# for i in strings:
+#     # increment value if element found init
+#     c += 1
+
+#     print(i)
+
+# print("length of sting is: ", c)
+
+# #############################################################
+# #############################################################
+
+# rstring = input("Enter a string: ")
+# r = ""
+# for i in rstring:
+#     r = r + i
+
+# print("Reversed string is: ", r)
+
+# #############################################################
+# #############################################################
+
+a = "Hello Shivam"
+print(a)
+a = a.replace("o", "")
+print(a)
